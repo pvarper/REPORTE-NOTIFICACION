1ac806ac5c7253dc4b6578776369caeb60497a89
pedro <p_vargasp@hotmail.com>
2015-03-28 08:50:04 -0400

    PRIMERA

---
+++ src/micrium/calldetail/test/Test.java
@@ -0,0 +1,17 @@
+package micrium.calldetail.test;
+
+import java.io.File;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class Test {
+
+	//private static final Logger log = Logger.getLogger(Retry.class);
+	public static int ses=0;
+	public static void main(String[] args) {
+		DOMConfigurator.configure("etc" + File.separator + "log4j.xml");
+	//	Retry.retryTBol();
+		
+	}
+	
+
+}
